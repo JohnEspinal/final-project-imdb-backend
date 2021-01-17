a1e3caf35e4cf08c78b7ebddd6abb66633f67ea4
Jose <jose.berges23@gmail.com>
2021-01-17 16:27:42 -0400

    Add test files

---
+++ backend/src/index.test.js
@@ -0,0 +1,5 @@
+//Agregar aqui las pruebas de index.js
+
+test('prueba', () => {
+  expect(0).toBe(0);
+});
---
+++ backend/src/routes/movies.test.js
@@ -0,0 +1,5 @@
+//Agregar aqui las pruebas de movies.js
+
+test('prueba', () => {
+  expect(0).toBe(0);
+});
